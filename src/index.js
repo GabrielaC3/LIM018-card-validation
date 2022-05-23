a45f37cef3b76bf27c52b95b7532f1eab2484820
Gabriela Córdova <gabycr.3.2.gcr@gmail.com>
2022-05-23 09:26:03 -0500

    Probando

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+
+
+
+
+import primerPaso from './validator.js';
+
+
+//let limpiar = document.getElementById("limpiar");
+
+//limpiar.addEventListener("click", clean);
+
+function clean(){
+    document.getElementById('reg').reset();
+}
+
