38af0c3bbce7b3ccd422d40334d791adf0509f84
Gabriela Córdova <gabycr.3.2.gcr@gmail.com>
2022-05-25 18:49:38 -0500

    Avances al 25-05-2022

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import validator from './validator.js';
 
-console.log(validator);
 
 
+let limpiar = document.getElementById("limpiar");
+limpiar.addEventListener("click", clean);
+
+function clean(){
+    document.getElementById('reg').reset();
+}
+
+let btnRegistrar = document.getElementById('registrar');
+btnRegistrar.addEventListener ('click', regis);
+
+
+function regis() {
+    let cad = document.getElementById('tar').value;
+    console.log(cad);
+    console.log(validator.isValid(cad));
+}
 
--- src/validator.js
+++ src/validator.js
@@ -1,6 +1,50 @@
 const validator = {
-  // ...
+  
 };
 
-export default validator;
+validator.isValid = function(creditCardNumber){
+        
+    let nuevo = [];
+    
+    for (let inv=creditCardNumber.length-1 ; inv>=0 ; inv=inv-1){
+      nuevo = nuevo + creditCardNumber[inv];
+     }
+    
+    let arreglo = nuevo.split('');
+    let nuevo1 = [];
+    nuevo1 = arreglo;
+    for (let pos=1 ; pos<arreglo.length ; pos=pos+2){
+      nuevo1[pos]=(parseInt(arreglo[pos])*2).toString();
+     }    
+    
+    let paso2 = [];
+    paso2 = nuevo1;
+    let nuevo2 = paso2
+    
+    for(let i=0 ; i<paso2.length ; i= i+1){
+      if(paso2[i]>9){
+        let temp = paso2[i];
+        let sum = parseInt(temp[0]) + parseInt(temp[1]);
+        nuevo2[i]=sum.toString();
+      }else {
+        nuevo2[i]=paso2[i];
+      }
+    }
 
+    let paso3 = [];
+    paso3 = nuevo2;
+    let suma2 = 0;
+
+    for(let j=0 ; j<paso3.length ; j=j+1){
+      suma2 = suma2 + parseInt(paso3[j]);
+    }
+    
+    if(suma2%10==0){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+export default validator;
