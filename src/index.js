8c13accc5e3b1466edd010d15635e0511edcfb37
Gabriela Córdova <gabycr.3.2.gcr@gmail.com>
2022-05-27 22:46:48 -0500

    Algunas validaciones

--- src/index.js
+++ src/index.js
@@ -8,26 +8,59 @@ limpiar.addEventListener("click", clean);
 function clean(){
     document.getElementById('reg').reset();
     document.getElementById('respuesta').innerHTML= '';
+    document.getElementById('errort').innerHTML = '';
+    document.getElementById('errori').innerHTML = '';
+    document.getElementById('errorb').innerHTML = '';
 }
 
 let btnRegistrar = document.getElementById('registrar');
 btnRegistrar.addEventListener ('click', regis);
 
 
-function regis() {
+
+function validForm() {
     let cad = document.getElementById('tar').value;
+    let identify = document.getElementById('ident').value;
+    let bove = document.getElementById('bov').value;
 
-    let inputNombre = document.getElementById('tar');
-    inputNombre.value = validator.maskify(cad);
-
-    //let labelRespuesta = document.getElementById('respuesta');
-
-    if(validator.isValid(cad)==true){
-        document.getElementById('respuesta').innerHTML= 'Su tarjeta es válida';
-    } else {
-        document.getElementById('respuesta').innerHTML= '¡Su tarjeta es inválida! Por favor registre un número de tarjeta válido';
+    if(cad === null || cad ===''){
+        document.getElementById('errort').innerHTML = 'Debe ingresar un número de tarjeta';
     }
 
+    if(identify === null || identify ===''){
+        document.getElementById('errori').innerHTML = 'Debe ingresar un número de identificación';
+    }
+
+    if(bove === null || bove===''){
+        document.getElementById('errorb').innerHTML = 'Debe ingresar un número de bóveda';
+    }
+
+    return false;
 }
+
+function regis() {
+
+    let cad = document.getElementById('tar').value;
+    let inputCad = document.getElementById('tar');
+    inputCad.value = validator.maskify(cad);
+
+    if(isNaN(cad)==false){
+        if(cad.length==16){
+            if(validator.isValid(cad)==true){
+                document.getElementById('respuesta').innerHTML= 'Su tarjeta es válida';
+            } else {
+                document.getElementById('respuesta').innerHTML= '¡Su tarjeta es inválida! Por favor registre un número de tarjeta válido';
+            }
+        }else{
+            alert('¡Error! Su tarjeta debe tener 16 digitos para poder validarla');
+        }
+    }else{
+        alert('¡Error! Solo debe ingresar números para la validación');
+    }
+
+    return validForm();
+    
+    }
+
 
 
